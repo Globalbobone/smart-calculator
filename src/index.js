92e94f6046db71f1ab6a2b8d6390556e4f11966f
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-02-23 14:10:32 +0300

    add part of solution -16 tests

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ class SmartCalculator {
   constructor(initialValue) {
     this.stack = [];
     this.stack.push(initialValue);
-    this.result = initialValue;
+    this.value = 0;
   }
 
   add(number) {
     this.stack.push('+');
     this.stack.push(number);
     let s = this.stack.join('');
-    return eval(s);
+    this.value = eval(s);
+    return this;
     //return this.result = this.result + number;
   }
   
@@ -17,29 +18,37 @@ class SmartCalculator {
     this.stack.push('-');
     this.stack.push(number);
     let s = this.stack.join('');
-    return eval(s);
+    this.value = eval(s);
+    return this;
   }
 
   multiply(number) {
     this.stack.push('*');
     this.stack.push(number);
     let s = this.stack.join('');
-    return eval(s);
+    this.value = eval(s);
+    return this;
   }
 
   devide(number) {
     this.stack.push('/');
     this.stack.push(number);
     let s = this.stack.join('');
-    return eval(s);
+    this.value = eval(s);
+    return this;
   }
 
   pow(number) {
     let p = Math.pow(this.stack.pop(), number);
-    this.stack.push('+');
+    //this.stack.push('+');
     this.stack.push(p);
     let s = this.stack.join('');
-    return eval(s);
+    this.value = eval(s);
+    return this;
+  }
+
+  valueOf() {
+    return this.value;
   }
 }
 
